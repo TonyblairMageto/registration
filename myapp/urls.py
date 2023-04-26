503a28450ec6016fbefa84dc69c0efba1171cd8e
tishtito <tishtito01@gmail.com>
2023-04-26 15:46:31 +0300

    first commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('home/', views.home, name='home'),
--- myapp/views.py
+++ myapp/views.py
@@ -1,25 +1,26 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import Member
 
 
 # Create your views here.
 def register(request):
-    if request.method=='post':
-        member=Member(firstname=request.post['firstname'],lastname=request.post['lastname'],password=request.post['password'])
+    if request.method == 'POST':
+        member = Member(firstname=request.POST['firstname'], lastname=request.POST['lastname'], username=request.POST['username'],
+                        password=request.POST['password'])
         member.save()
         return redirect('/')
     else:
-        return render(request, 'register.html',)
+        return render(request, 'register.html', )
+
 
 def login(request):
     return render(request, 'login.html')
 
+
 def home(request):
-    if request.method == 'post':
-       if Member.objects.filter(username=request.post['username'], password=request.post['password']).exists():
-           member = Member.objects.get(username=request.post['username'], password=request.post['password'])
-           return render(request, 'home.html', {'member': member})
-       else:
-             return render(request, 'login.html')
-
-
+    if request.method == 'POST':
+        if Member.objects.filter(username=request.POST['username'], password=request.POST['password']).exists():
+            member = Member.objects.get(username=request.POST['username'], password=request.POST['password'])
+            return render(request, 'home.html', {'member': member})
+        else:
+            return render(request, 'login.html')
